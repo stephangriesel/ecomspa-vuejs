f3f94a27820192bacb600ec03e49d38947e377e4
Stephan <sgriesel@gmail.com>
2021-07-27 06:04:43 +0200

    set up structure

--- src/main.js
+++ src/main.js
@@ -1,4 +1,40 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
+import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client/core';
+import VueApollo from '@vue/apollo-option';
+import ProductComponent from './components/Product';
+import ProductsComponent from './components/Products';
+import {createRouter, createWebHashHistory} from 'vue-router';
 
-createApp(App).mount('#app')
+const httpLink = createHttpLink({
+  uri:
+    'https://api-ca-central-1.graphcms.com/v2/ckpn42kbid0wf01xsfl4ehoq0/master',
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
+const routes = [
+  {path: '/', component: ProductsComponent},
+  {path: '/product/:id', component: ProductComponent},
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: routes,
+});
+
+createApp(App)
+  .use(router)
+  .provide('apollo', apolloProvider)
+  .mount('#app');
