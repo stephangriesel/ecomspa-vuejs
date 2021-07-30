ff5175c38350044c38e87b9b4abf81f2beaf9f1a
Stephan <sgriesel@gmail.com>
2021-07-30 06:56:25 +0200

    add layout components

---
+++ src/Layout/Base.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <nav class="navbar">
+        <router-link class="logo" to="/">VueShop</router-link>
+        <div>
+          <div v-if="user.photoURL">
+            <img :src="user.photoURL" class="" alt />
+            <router-link class="btn" to="/cart">
+              <img
+                src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png"
+                width="50"
+                alt
+              />
+              <span class="badge">{{ cart.length }}</span>
+            </router-link>
+          </div>
+          <router-link v-else class="btn" to="/login">Login</router-link>
+        </div>
+      </nav>
+    </div>
+    <div class="page-wrapper">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import {mapGetters} from 'vuex';
+export default {
+  name: 'Base',
+  computed: {
+    ...mapGetters('account', ['user']),
+    ...mapGetters('product', ['cart']),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/home/ProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <img :src="product.imageUrl" class="product-image" />
+    <div class="card-body">
+      <h5 class="card-title">{{ product.name }}</h5>
+      <div class="item">
+        <router-link type="button" class="btn" :to="'/details/' + product.id"
+          >Details</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProductCard',
+  props: ['product'],
+};
+</script>
+<style>
+.card .product-image {
+  height: 300px;
+}
+</style>
